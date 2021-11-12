5ccbf85c298412712b1867228014f7a7c1d951ec
StavitskiyA <Alexey.Stavitskiy@cse.ru>
2021-11-12 10:41:59 +0300

    Первичная загрузка. Рабочий вариант ТОМИЛИНО. Перенес из другого репозитория.

---
+++ src/main/java/ru/cse/proxysorter/Processors/Res12Agregate.java
@@ -0,0 +1,16 @@
+package ru.cse.proxysorter.Processors;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.processor.aggregate.AggregationStrategy;
+import ru.cse.proxysorter.Message.Request11;
+
+public class Res12Agregate implements AggregationStrategy {
+    @Override
+    public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+
+        Request11 originalBody = oldExchange.getIn().getBody(Request11.class);
+
+        newExchange = oldExchange;
+        return newExchange;
+    }
+}
